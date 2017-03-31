ac4d8b60ae41a500d69ee94dbdce37cb13786954
deshkor <deshkor@protonmail.com>
2017-03-31 19:55:18 +0100

    Fix gulp build and Dockerfile

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -8,10 +8,19 @@ var $ = require('gulp-load-plugins')();
 gulp.task('help', $.taskListing);
 gulp.task('default', ['help']);
 
-gulp.task('build', ['clean'])
+gulp.task('build', ['clean', 'build-files'])
 
 gulp.task('clean', function (done) {
   del.sync(['.tmp/**/*', 'dist/**/*']);
   done();
+});
+
+gulp.task('build-files', [], function() {
+	return gulp
+		.src([
+			'src/**/*',
+			'!src/bower_components/**/*'
+		])
+		.pipe(gulp.dest('dist'))
 });
 
